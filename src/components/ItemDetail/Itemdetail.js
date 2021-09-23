f9439cc96719ea6009a34938e58b526d375e49f3
miansonu028989 <miansonu028989@gmail.com>
2021-09-23 11:18:55 -0700

    ""

--- src/components/ItemDetail/Itemdetail.js
+++ src/components/ItemDetail/Itemdetail.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react'
-
+import { Link } from 'react-router-dom'
 
 export default class ItemsDetail extends Component {
     constructor(props) {
@@ -13,16 +13,16 @@ export default class ItemsDetail extends Component {
         
         const data = await fetch(`https://fortnite-api.theapinetwork.com/item/get?id=${this.props.match.params.id}`)
         const items= await data.json();
-        console.log(this.props.match);
+        console.log(items);
+        this.setState({items:items.data.item})
+        console.log(this.state.items);
     }
 
     render() {
         return (
             <div>
             <button type="button" onClick={this.imagefatch}>Show</button>
-                <h1>
-                 item
-                </h1>
+                <h1>{this.state.items.name}</h1>
             </div>
         )
     }
